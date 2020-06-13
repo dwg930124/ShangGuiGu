624c0a8111e5c6d7278677d254f5255a239fa1c8
dwg930124 <dwg691013@163.com>
2020-06-13 15:25:41 +0800

    View的创建

--- Project02/src/com/dwg/ui/CustomerView.java
+++ Project02/src/com/dwg/ui/CustomerView.java
@@ -1,11 +1,55 @@
 package com.dwg.ui;
 
+import com.dwg.service.CustomerList;
+
 /**
+ * 负责菜单的显示和处理用户操作
  * @author DWG
  * @create 2020-06-13-11:23
  */
 public class CustomerView {
-    public static void main(String[] args) {
-        System.out.println("-----------客户信息管理软件--------");
+    private  CustomerList customerList = new CustomerList(10);
+
+    /**
+     * 显示主菜单，响应用户输入，根据用户操作分别调用其他相应的成员方法
+     */
+    public void enterMainMenu(){
+
+    }
+
+    /**
+     * 添加客户
+     */
+    private void addNewCustomer(){
+
+    }
+
+    /**
+     * 修改客户
+     */
+    private void modifyCustomer(){
+
+    }
+
+    /**
+     * 删除客户
+     */
+    private void deleteCustomer(){
+
+    }
+
+    /**
+     * 客户列表
+     */
+    private void listAllCustomers(){
+
+    }
+
+    /**
+     * 创建CustomerView实例，并调用 enterMainMenu()方法以执行程序
+     * @param args
+     */
+    public static void main(String[] args){
+
     }
 }
